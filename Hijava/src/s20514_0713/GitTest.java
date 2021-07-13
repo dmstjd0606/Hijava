60578d2e059a3d670a60a5046ec5c175d720f68a
leeeunseong <leeeunseong@SEC>
2021-07-13 10:34:44 +0900

    두번쨰 깃허브 소스

--- Hijava/src/s20514_0713/GitTest.java
+++ Hijava/src/s20514_0713/GitTest.java
@@ -5,6 +5,8 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("¾È³ç ±êÇãºê Ã¹¹ø¤Š");
+		
+		System.out.println("µÎ¹ø¤Š ±êÇãºê");
 
 	}
 
