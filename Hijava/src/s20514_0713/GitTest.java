ee8e27567102a36096e647cb6accfba71af45633
leeeunseong <leeeunseong@SEC>
2021-07-13 10:22:03 +0900

    첫번쨰깃허브소스

---
+++ Hijava/src/s20514_0713/GitTest.java
@@ -0,0 +1,11 @@
+package s20514_0713;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¾È³ç ±êÇãºê Ã¹¹ø¤Š");
+
+	}
+
+}
